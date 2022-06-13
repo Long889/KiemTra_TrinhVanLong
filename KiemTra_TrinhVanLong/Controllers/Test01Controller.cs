c4e7fc2aa8663cfdbeb83d7aa9805d9eddbbd04d
trinhvanlong889 <trinhvanlong889@gmail.com>
2022-06-13 19:28:53 +0700

    up

--- KiemTra_TrinhVanLong/Controllers/Test01Controller.cs
+++ KiemTra_TrinhVanLong/Controllers/Test01Controller.cs
@@ -21,27 +21,27 @@ namespace KiemTra_TrinhVanLong.Controllers
             var sinhVien = data.SinhViens.Where(m => m.MaSV == id).First();
             return View(sinhVien);
         }
-        public ActionResult Create()
+        public ActionResult Create(string id)
         {
             return View();
         }
         [HttpPost]
-        public ActionResult Create(FormCollection collection, SinhVien s)
+        public ActionResult Create(string id, FormCollection collection, SinhVien s)
         {
-            var E_MaSV = collection["MaSV"];
-            var E_HoTen = collection["tensinhvien"];
+            var E_masv = s.MaSV;
+            var E_hoten = collection["hoten"];
             var E_gioitinh = collection["gioitinh"];
             var E_ngaysinh = Convert.ToDateTime(collection["ngaysinh"]);
             var E_hinh = collection["hinh"];
             var E_manganh = collection["manganh"];
-            if (string.IsNullOrEmpty(E_MaSV))
+            if (string.IsNullOrEmpty(E_hoten))
             {
                 ViewData["Error"] = "Don't empty!";
             }
             else
             {
-                s.MaSV = E_MaSV;
-                s.HoTen = E_HoTen;
+                s.MaSV = E_masv;
+                s.HoTen = E_hoten.ToString();
                 s.GioiTinh = E_gioitinh.ToString();
                 s.NgaySinh = E_ngaysinh;
                 s.Hinh = E_hinh.ToString();
@@ -50,35 +50,36 @@ namespace KiemTra_TrinhVanLong.Controllers
                 data.SubmitChanges();
                 return RedirectToAction("Index");
             }
-            return this.Create();
+            return this.Create(id);
         }
         public ActionResult Edit(string id)
         {
             var sinhVien = data.SinhViens.First(m => m.MaSV == id);
             return View(sinhVien);
         }
+        
         [HttpPost]
         public ActionResult Edit(string id, FormCollection collection)
         {
-            var E_MaSV = data.SinhViens.First(m => m.MaSV == id);
-            var E_HoTen = collection["tensinhvien"];
+            var E_sinhvien = data.SinhViens.First(m => m.MaSV == id);
+            var E_hoten = collection["hoten"];
             var E_gioitinh = collection["gioitinh"];
             var E_ngaysinh = Convert.ToDateTime(collection["ngaysinh"]);
             var E_hinh = collection["hinh"];
             var E_manganh = collection["manganh"];
-            E_MaSV.MaSV = id;
-            if (string.IsNullOrEmpty(E_HoTen))
+            E_sinhvien.MaSV = id;
+            if (string.IsNullOrEmpty(E_hoten))
             {
                 ViewData["Error"] = "Don't empty!";
             }
             else
             {
-                E_MaSV.HoTen = E_HoTen; 
-                E_MaSV.GioiTinh = E_gioitinh;
-                E_MaSV.NgaySinh = E_ngaysinh;
-                E_MaSV.Hinh = E_hinh;
-                E_MaSV.MaNganh = E_manganh;
-                UpdateModel(E_MaSV);
+                E_sinhvien.HoTen = E_hoten;
+                E_sinhvien.GioiTinh = E_gioitinh;
+                E_sinhvien.NgaySinh = E_ngaysinh;
+                E_sinhvien.Hinh = E_hinh;
+                E_sinhvien.MaNganh = E_manganh;
+                UpdateModel(E_sinhvien);
                 data.SubmitChanges();
                 return RedirectToAction("Index");
             }
